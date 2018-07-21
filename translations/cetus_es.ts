025172ec6160a6a0bcf403e45dae834d826e5f8e
Alexander Rössler <mail@roessler.systems>
2018-07-21 14:43:41 +0200

    adding Spanish translation files

---
+++ translations/cetus_es.ts
@@ -0,0 +1,351 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <source>About Cetus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationMenuBar</name>
+    <message>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Disconnect from Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sh&amp;utdown Session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>E&amp;xit User Interface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Machine</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show o&amp;ffsets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show v&amp;elocity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show &amp;distance to go</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable &amp;preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sh&amp;ow machine limits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>S&amp;how program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show program e&amp;xtents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show program r&amp;apids</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alpha-&amp;blend program</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sho&amp;w live plot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sho&amp;w tool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show &amp;coordinate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About &amp;Cetus</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AxisControls</name>
+    <message>
+        <source>Axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Jog Velocity</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Cetus</name>
+    <message>
+        <source>DRO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Webcam</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoolantControls</name>
+    <message>
+        <source>Coolant</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DisplayPanel</name>
+    <message>
+        <source>Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ErrorTab</name>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FeedrateControl</name>
+    <message>
+        <source>Feed Override</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MachineStatusLabel</name>
+    <message>
+        <source>ESTOP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OFF</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>ON</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ManualTab</name>
+    <message>
+        <source>Manual</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MaximumVelocityControl</name>
+    <message>
+        <source>Maximum Velocity</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MdiTab</name>
+    <message>
+        <source>MDI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Active GCodes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PositionOffsetLabel</name>
+    <message>
+        <source>Position: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Relative Actual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Machine Actual</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PreviewPanel</name>
+    <message>
+        <source>Enable preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PrinterTab</name>
+    <message>
+        <source>Printer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>E0 temperature - set to </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bed temperature - set to </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fan speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove Filament</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert Filament</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Calibrate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Probe</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RapidrateControl</name>
+    <message>
+        <source>Rapid Override</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SpindleControls</name>
+    <message>
+        <source>Spindle</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SpindlerateControl</name>
+    <message>
+        <source>Spindle Override</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StyledDroBase</name>
+    <message>
+        <source>DRO</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Distance to Go</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rad</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dia</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Extra</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Vel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DTG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 Offsets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>G92 Offsets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tool Offsets</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ToolStatusLabel</name>
+    <message>
+        <source>Tool %1, offset: %2, diameter: %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No Tool</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WebcamPanel</name>
+    <message>
+        <source>No webcam connected.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Turn on the switch.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
